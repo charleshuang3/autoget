f9130d746dcfc7a3eedbe81b99f2c00b403442e4
charles.huang <charleshuang233@gmail.com>
2024-11-09 23:29:07 -0500

    [backend][main] add main func

---
+++ backend/cmd/daemon/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/charleshuang3/autoget/backend/config"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: daemon <config-path>")
+	}
+
+	configPath := os.Args[1]
+
+	_, err := config.LoadConfig(configPath)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %s\n", err)
+	}
+
+}
---
+++ prompts/3-backend-main.md
@@ -0,0 +1,1 @@
+help me write backend/cmd/daemon/main.go: it should accept exact 1 argument as path of config then load config
