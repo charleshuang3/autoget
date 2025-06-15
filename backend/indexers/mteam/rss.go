9d85563fd1e535e5977355184ca0e9492d87c978
charleshuang3 <charleshuang233@gmail.com>
2025-06-15 09:52:28 -0400

    [indexers] parseRSSItem for mteam

--- backend/indexers/mteam/rss.go
+++ backend/indexers/mteam/rss.go
@@ -2,7 +2,9 @@ package mteam
 
 import (
 	"github.com/charleshuang3/autoget/backend/indexers"
+	"github.com/charleshuang3/autoget/backend/internal/db"
 	"github.com/charleshuang3/autoget/backend/internal/errors"
+	"github.com/mmcdole/gofeed"
 	"github.com/robfig/cron/v3"
 )
 
@@ -12,4 +14,15 @@ func (m *MTeam) RegisterSearchForRSS(s *indexers.RSSSearch) *errors.HTTPStatusEr
 
 func (m *MTeam) RegisterRSSCronjob(cron *cron.Cron) {
 
+}
+
+func (m *MTeam) parseRSSItem(item *gofeed.Item, target *db.RSSSearch) {
+	target.ResID = item.GUID
+	target.Title = item.Title
+	if len(item.Categories) > 0 {
+		target.Catergory = item.Categories[0]
+	}
+	if len(item.Enclosures) > 0 {
+		target.URL = item.Enclosures[0].URL
+	}
 }
---
+++ backend/indexers/mteam/rss_test.go
@@ -0,0 +1,39 @@
+package mteam
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/charleshuang3/autoget/backend/internal/db"
+	"github.com/mmcdole/gofeed"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	//go:embed test_data/rss.xml
+	rssResp string
+)
+
+func TestParseRSSItem(t *testing.T) {
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseString(rssResp)
+	require.NoError(t, err)
+	require.Len(t, feed.Items, 2)
+
+	m := NewMTeam(&Config{
+		APIKey: "api-key",
+	}, "", nil, nil)
+
+	got := &db.RSSSearch{}
+	m.parseRSSItem(feed.Items[0], got)
+
+	want := &db.RSSSearch{
+		ResID:     "111111",
+		Title:     "Match Search 1",
+		Catergory: "AV(無碼)/HD Uncensored",
+		URL:       "https://rss.m-team.cc/api/rss/dlv2?uid=111111",
+	}
+
+	assert.Equal(t, want, got)
+}
